d3dfb9e80febb36729a47887bae9757e44e096c1
harikumarmanickam <40386303+harikumarmanickam@users.noreply.github.com>
2023-06-13 17:11:57 +0530

    Code changed

--- main.tf
+++ main.tf
@@ -1,19 +1,20 @@
 /* Test creating VM and eliminate to Run */
 
-resource "azurerm_resource_group" "example1" {
-  name     = "example1"
-  location = "East US"
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
 }
+
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
-  location            = "East US"
-  resource_group_name = "example1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "example" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group_name
+  resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
