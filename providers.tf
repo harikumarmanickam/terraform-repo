2a623ab86425c3782b5adf486f9807cf20f2503f
harikumarmanickam <40386303+harikumarmanickam@users.noreply.github.com>
2023-06-13 12:58:12 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,7 +6,13 @@ terraform {
     }
   }
 }
-
+backend "azurerm" {
+    resource_group_name  = "StorageAccount-ResourceGroup"
+    storage_account_name = "abcd1234"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+}
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   use_oidc = true
